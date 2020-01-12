c10551e25d775b0d34a863b9e39131348820d681
Daniel Mayusa Dewa <danielmayusadewa@gmail.com>
2020-01-12 23:28:08 +0700

    fix fetch json

--- src/utils/fetchJson.js
+++ src/utils/fetchJson.js
@@ -1,33 +1,77 @@
-import React, { Component } from "react";
 import axios from "axios";
 
-const API = "http://rest.learncode.academy/api/ucon/news/";
-const DEFAULT_QUERY = "redux";
+type Method = "GET" | "POST" | "PUT" | "PATCH";
 
-class fetchJson extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hits: [],
-      isLoading: false,
-      error: null
-    };
-  }
-
-  async componentDidMount() {
-    this.setState({ isLoading: true });
-    try {
-      const result = await axios.get(API + DEFAULT_QUERY);
-      this.setState({
-        hits: result.data.hits,
-        isLoading: false
+export default async function fetchJson(
+  method: Method,
+  path: string,
+  data: any
+): any {
+  try {
+    const token = await localStorage.getItem("@token");
+    // const token = 123;
+    if (token != undefined) {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}${path}`, {
+        method,
+        headers: {
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+          // Authorization: token,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
       });
-    } catch (error) {
-      this.setState({
-        error,
-        isLoading: false
+      const result = await response.json();
+      if (result.error != null) {
+        throw result.error;
+      } else {
+        return result;
+      }
+    } else {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}${path}`, {
+        method,
+        headers: {
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
       });
+      const result = await response.json();
+      if (result.error != null) {
+        throw result.error;
+      } else {
+        return result;
+      }
     }
+  } catch (error) {
+    throw error;
   }
 }
-export default fetchJson;
+
+export async function fetchFormData(path, imageFile) {
+  // console.log(imageFile);
+  const url = `${process.env.REACT_APP_API_URL}${path}`;
+  try {
+    const formData = new FormData();
+    if (imageFile instanceof Array) {
+      await imageFile.map(item => {
+        formData.append("image", item);
+      });
+    } else {
+      formData.append("image", imageFile);
+    }
+
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        "Access-Control-Allow-Origin": "*"
+      }
+    };
+    const response = await axios.post(url, formData, config);
+    const result = await response.data;
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
