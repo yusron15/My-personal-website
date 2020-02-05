b843708a0ac657e4a03ac859b858aa9106c114e6
Billy Damara Hidayat <billydamara@gmail.com>
2020-02-05 16:53:52 +0700

    Update Gold.jsx

--- src/views/layout/Gold.jsx
+++ src/views/layout/Gold.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
+
 import ColoredNavbar from "../../components/Navbars/ColoredNavbar.jsx";
 import Content from "../../components/Gold/Content.jsx";
 import Footer from "../../components/Footers/Footer.jsx";
 
+import { fetchPage, getContent, getNews } from "../../redux/ducks/actions.js";
+
 class Gold extends React.Component {
   state = {};
-  componentDidMount = () => {
+  componentDidMount = async () => {
+    await this.props.getContent("Gold", this.props.currentLang, true);
     window.scroll(0, 0);
   };
   render() {
@@ -22,4 +27,15 @@ class Gold extends React.Component {
   }
 }
 
-export default Gold;
+const mapStateToProps = state => ({
+  pageStore: state.pageStore,
+  currentLang: state.pageStore.currentLang
+});
+
+const mapDispatchToProps = dispatch => ({
+  // fetchPage: (section, lang) => dispatch(fetchPage(section, lang)),
+  getContent: (section, lang, toggle) =>
+    dispatch(getContent(section, lang, toggle))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Gold);
