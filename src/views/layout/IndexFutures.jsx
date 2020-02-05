3769b3dd1a7ad71af373460ed55ad14bedd178d6
Billy Damara Hidayat <billydamara@gmail.com>
2020-02-05 17:02:29 +0700

    Update IndexFutures.jsx

--- src/views/layout/IndexFutures.jsx
+++ src/views/layout/IndexFutures.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
+
 import Footer from "../../components/Footers/Footer.jsx";
 import Content from "../../components/IndexFutures/Content.jsx";
+
+import { fetchPage, getContent, getNews } from "../../redux/ducks/actions.js";
+
 class IndexFutures extends React.Component {
   state = {};
-  componentDidMount = () => {
+  componentDidMount = async () => {
+    await this.props.getContent("indexfutures", this.props.currentLang, true);
     window.scroll(0, 0);
   };
   render() {
@@ -16,4 +22,15 @@ class IndexFutures extends React.Component {
   }
 }
 
-export default IndexFutures;
+const mapStateToProps = state => ({
+  pageStore: state.pageStore,
+  currentLang: state.pageStore.currentLang
+});
+
+const mapDispatchToProps = dispatch => ({
+  // fetchPage: (section, lang) => dispatch(fetchPage(section, lang)),
+  getContent: (section, lang, toggle) =>
+    dispatch(getContent(section, lang, toggle))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(IndexFutures);
