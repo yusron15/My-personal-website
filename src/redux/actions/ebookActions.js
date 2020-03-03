8ae589c61e97c6f64148b4ad451183052ce8cb24
Billy Damara Hidayat <billydamara@gmail.com>
2020-03-04 02:18:31 +0700

    Update ebook and privacy policy

--- src/redux/actions/ebookActions.js
+++ src/redux/actions/ebookActions.js
@@ -3,9 +3,8 @@ import { SubscribeEbook } from "../api/ebook";
 export function subscribeEbook(data) {
   return async dispatch => {
     let result = await SubscribeEbook(data);
-    console.log(result, "asdscaada");
     dispatch({
-      type: `GET_CABANGS`,
+      type: ``,
       payload: result
     });
 
--- src/views/layout/PrivacyPolicy.jsx
+++ src/views/layout/PrivacyPolicy.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FileViewer from "react-file-viewer";
 
 export default () => {
+  const [fileUrl, setFileUrl] = useState("");
+  const fetchPrivacyPolicy = async () => {
+    try {
+      let result = await fetch(
+        // "https://api-topgrowthfutures.kool.co.id/api/filebuckets?filter[where][type]='privacyPolicy'"
+        "http://localhost:3000/api/filebuckets?filter[where][type]=privacyPolicy"
+      );
+
+      result = await result.json();
+
+      console.log(result);
+
+      await setFileUrl(result[0].fileUrl);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchPrivacyPolicy();
+  }, []);
+
   return (
     <div
       className="file-viewer"
-      style={{ height: "100%", width: "100%", backgroundColor: "grey" }}
+      style={{
+        backgroundColor: "grey",
+        width: "100%",
+        height: "100%"
+
+        // display: "flex",
+        // justifyContent: "center"
+      }}
     >
-      <FileViewer
-        fileType={"docx"}
-        filePath={"https://topgrowth.sgp1.digitaloceanspaces.com/FAQ.docx"}
-      />
+      {fileUrl && <FileViewer fileType={"pdf"} filePath={fileUrl} />}
     </div>
   );
 };
